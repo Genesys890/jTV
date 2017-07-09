23ec4c23cdf84f2feac831dbbc231d4f693377a5
rolmedo <rolmedo@rolmedo-desktop>
2017-07-09 12:30:13 +0200

    Ready to start

--- src/jtv/JTV.java
+++ src/jtv/JTV.java
@@ -5,6 +5,11 @@
  */
 package jtv;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import uk.co.caprica.vlcj.binding.LibVlc;
+import uk.co.caprica.vlcj.discovery.NativeDiscovery;
+
 /**
  *
  * @author Genesys890
@@ -14,8 +19,12 @@ public class JTV {
     /**
      * @param args the command line arguments
      */
+    private static final Logger _log = LoggerFactory.getLogger(JTV.class);
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        boolean found = new NativeDiscovery().discover();
+        System.out.println(found);
+        System.out.println(LibVlc.INSTANCE.libvlc_get_version());
     }
     
 }
